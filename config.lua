8372a3c0b513561645a98258e9a6c80969752e25
mrskunky69 <40913528+mrskunky69@users.noreply.github.com>
2024-07-31 18:24:07 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,19 +1,21 @@
 Config = {}
 
-Config.BLUEZONE = {x = -244.81, y = 663.72, z = 113.35}
-Config.REDZONE = {x = -228.56, y = 667.53, z = 113.38}
-Config.CENTER = {x = -234.91, y = 665.78, z = 113.30}
+Config.BLUEZONE = vector3(-244.81, 663.72, 113.35)
+Config.REDZONE = vector3(-228.56, 667.53, 113.38)
+Config.CENTER = vector3(-234.91, 665.78, 113.30)
 
-Config.DISTANCE = 10 
-Config.TP_DISTANCE = 6 
-Config.LEAVE_FIGHT_DISTANCE = 15 
-Config.DISTANCE_INTERACTION = 2.0 
-Config.E_KEY = 0xCEFD9220 
-Config.WINNER_REWARD = 50 
+Config.DISTANCE = 10
+Config.TP_DISTANCE = 6
+Config.LEAVE_FIGHT_DISTANCE = 15
+Config.DISTANCE_INTERACTION = 2.0
+Config.E_KEY = 0xCEFD9220
+Config.G_KEY = 0x760A9C6F  -- G key
+Config.WINNER_REWARD = 50
 Config.BLIP = {
-    coords = {x = -234.91, y = 665.78, z = 113.30},
+    coords = vector3(-234.91, 665.78, 113.30),
     text   = 'fightclub',
     sprite = -1735903728,
     color  = 1,
-    scale  = 0.8
+    scale  = 0.2
 }
+
